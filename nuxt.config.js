0bc4fc6a54d020cdf85fde70c4e7ba489f014ae7
Qendolin <32160662+Qendolin@users.noreply.github.com>
2021-04-05 20:59:06 +0200

    initialized nuxt ssr & firebase project

---
+++ nuxt.config.js
@@ -0,0 +1,73 @@
+export default {
+	// Target: https://go.nuxtjs.dev/config-target
+	target: 'static',
+
+	// Global page headers: https://go.nuxtjs.dev/config-head
+	head: {
+		title: 'wendelin-muth',
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{ hid: 'description', name: 'description', content: '' },
+		],
+		link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+	},
+
+	// Global CSS: https://go.nuxtjs.dev/config-css
+	css: [],
+
+	// Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+	plugins: [],
+
+	// Auto import components: https://go.nuxtjs.dev/config-components
+	components: true,
+
+	// Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+	buildModules: [
+		// https://go.nuxtjs.dev/typescript
+		'@nuxt/typescript-build',
+		// https://go.nuxtjs.dev/stylelint
+		'@nuxtjs/stylelint-module',
+	],
+
+	// Modules: https://go.nuxtjs.dev/config-modules
+	modules: [
+		// https://go.nuxtjs.dev/axios
+		'@nuxtjs/axios',
+		// https://go.nuxtjs.dev/pwa
+		'@nuxtjs/pwa',
+		[
+			'@nuxtjs/firebase',
+			{
+				config: {
+					apiKey: 'AIzaSyDJ8atqjyu3jXNoCgj8Zi8iAwTUqvWTljk',
+					authDomain: 'wendelin-muth.firebaseapp.com',
+					projectId: 'wendelin-muth',
+					storageBucket: 'wendelin-muth.appspot.com',
+					messagingSenderId: '982220326248',
+					appId: '1:982220326248:web:1d17c23b5c8e35a76b02dd',
+					measurementId: 'G-X73GPRXRWM',
+				},
+				services: {
+					auth: false,
+					analytics: true,
+					performance: true,
+				},
+				onFirebaseHosting: true,
+			},
+		],
+	],
+
+	// Axios module configuration: https://go.nuxtjs.dev/config-axios
+	axios: {},
+
+	// PWA module configuration: https://go.nuxtjs.dev/pwa
+	pwa: {
+		manifest: {
+			lang: 'en',
+		},
+	},
+
+	// Build Configuration: https://go.nuxtjs.dev/config-build
+	build: {},
+}
